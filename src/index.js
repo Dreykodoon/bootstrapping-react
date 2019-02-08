8552ca01d66968a1987c843e9fd759a114582ce4
Mihai Cioban <ciobanmihai@gmail.com>
2019-02-08 10:50:07 +0200

    Coupled react app to webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
 module.exports = {
     mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: '/',
+        filename: 'main.js'
+    },
     devServer: {
-        // inline: true,
+        inline: true,
         contentBase: './public',
         port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
     }
 };
