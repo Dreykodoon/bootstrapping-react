cd18d382af7267b523e7f845e9bbab331fc7ada5
Mihai Cioban <ciobanmihai@gmail.com>
2019-02-11 09:52:14 +0200

    Added animation on entering

--- src/js/App.js
+++ src/js/App.js
@@ -1,22 +1,46 @@
 import React from 'react';
+import {Transition} from 'react-transition-group';
 
 import Button from './Button';
 import Jumbotron from './Jumbotron';
 import Container from './Container';
 
 
-export default () => {
-    return (
-        <Jumbotron>
-            <Container>
-                <h1>Building React.js User Interfaces</h1>
-                <p>with Bootstrap and SASS.</p>
-                <p>
-                    <Button className="btn btn-primary" href="http://02geek.com" target="_blank" disabled />
-                    <Button className="btn btn-danger" target="_blank" disabled />
-                </p>
-            </Container>
-        </Jumbotron>
-        
-    );
-};
+class App extends React.Component {
+    state = {
+        show: false
+    };
+
+    componentDidMount() {
+        this.setState({show: true});
+    }
+
+    render() {
+        const { show } = this.state;
+
+        return (
+            <Transition in={show} timeout={200} unmountOnExit>
+                {(state) => {
+                    const className = `transition-style transition-${state}`;
+
+                    return (
+                        <div className={className}>
+                            <Jumbotron>
+                                <Container>
+                                    <h1>Building React.js User Interfaces</h1>
+                                    <p>with Bootstrap and SASS.</p>
+                                    <p>
+                                        <Button className="btn btn-primary" href="http://02geek.com" target="_blank" />
+                                        <Button className="btn btn-danger" target="_blank" />
+                                    </p>
+                                </Container>
+                            </Jumbotron>
+                        </div>
+                    )
+                }}
+            </Transition>
+        );
+    }
+}
+
+export default App;
